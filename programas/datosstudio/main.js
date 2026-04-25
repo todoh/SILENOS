5ad1bf8411d6c31b908fd10d9aaf62ce4f932726
Manuel Rodsua <silensystem@gmail.com>
2026-04-25 12:24:49 +0200

    Delete programas/datosstudio directory

--- programas/datosstudio/main.js
+++
@@ -1,9 +0,0 @@
-const Sys = window.parent.SystemConfig;
-const FS = window.parent; 
-
-const app = new DataStudio();
-
-// CORRECCIÓN: Exponemos 'app' de forma global para que tramas.canvas.js y la UI lo lean sin problema.
-window.app = app; 
-
-window.onload = () => app.init();
